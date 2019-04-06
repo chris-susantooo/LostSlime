614ce8528d5096417e0a6d19cb8e04422af0fb05
sca1998 <sca1998@gmail.com>
2019-04-07 02:46:25 +0800

    Hidden scroll bar

--- server/public/js/game.js
+++ server/public/js/game.js
@@ -3,9 +3,10 @@ var config = {
     parent: 'phaser-example',
     width: 1920,
     height: 1080,
+    pixelArt: true,
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
     scene: {
         preload: preload,
