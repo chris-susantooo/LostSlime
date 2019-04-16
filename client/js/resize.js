b3190fdbd43ebca7b8600207a2f6d5370c340d51
sca1998 <sca1998@gmail.com>
2019-04-16 21:50:40 +0800

    Added comment

--- client/js/resize.js
+++ client/js/resize.js
@@ -6,13 +6,14 @@ const context = canvas.getContext('2d');
 
 export function monitorSizeChange() {
 
-    //initialize canvas to window dimension, 16:9 aspect ratio
+    //initialize canvas to window dimension
     doResize();
     
     //attach listener to call doResize whenever window is resized
     $(window).on('resize', doResize);
 }
 
+//resize canvas to fit window, locks aspect ratio to 16:9
 function doResize() {
     if(Math.floor(window.innerWidth * 0.5625) <= window.innerHeight) {
         context.canvas.width = window.innerWidth;
