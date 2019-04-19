cb68f9a4080d951312a8fd6ac8cb24482175a3cb
pinkkyyy <pinky.wychau@gmail.com>
2019-04-19 16:46:05 +0800

    update

--- test.py
+++ test.py
@@ -1,36 +1,43 @@
-from pynput.keyboard import Key, Listener
+from pynput import keyboard
 import time
 import contextlib
 with contextlib.redirect_stdout(None):
     import pygame
-import keyboard
 import json
+
+fn = input()
 
 data = {}  
 data['song'] = []  
 
 begin = time.time()
 pygame.mixer.init()
-pygame.mixer.music.load("testing.ogg")
+pygame.mixer.music.load(fn)
 pygame.mixer.music.play()
+
+def get_key_name(key):
+    if isinstance(key, keyboard.KeyCode):
+        return key.char
+    else:
+        return str(key)
 
 def on_press(key):
     now = time.time() - begin
     print('{0} pressed'.format(
         key))
-    k = keyboard.read_key()
+    k = get_key_name(key)
     data['song'].append({  
     'key': k,
     'time': now,
     })
 
 def on_release(key):
-    if key == Key.esc:
+    if str(key) == 'Key.esc':
         # Stop listener
         return False
 
 # Collect events until released
-with Listener(
+with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
