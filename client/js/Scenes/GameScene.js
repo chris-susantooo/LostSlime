927d827361d4fe89fdc6c7880ed894572bddff2f
Rico Wan <ricowan2007@gmail.com>
2019-04-22 02:18:13 +0800

    added waitscene (w/o network) and modified joinscene

--- client/js/Scenes/GameScene.js
+++ client/js/Scenes/GameScene.js
@@ -2,13 +2,11 @@ import Scene from '../Scene.js';
 import { loadImage } from '../loaders.js';
 import { Entity } from '../Entity.js';
 import { Vec2, calScaledMid, getMousePos } from '../util.js';
-import TitleScene from './TitleScene.js';
-import WaitingRoomScene from './WaitingRoomScene.js';
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-export default class JoinRoomScene extends Scene {
+export default class GameScene extends Scene {
 
     constructor() {
         super();
@@ -16,6 +14,7 @@ export default class JoinRoomScene extends Scene {
         this.loadVisualAssets();
 
         this.setupMouseEvents();
+
     }
 
     setupMouseEvents() {
@@ -55,47 +54,19 @@ export default class JoinRoomScene extends Scene {
         }
     }
 
-    transition(target) {
-        if(target === 'join') {
-            console.log('join!');
-        } else if(target === 'create') {
-            let create = new WaitingRoomScene();
-            create.show();
-        } else if(target === 'arrow') {
-            let title = new TitleScene();
-            title.show();
-        }
-    }
-
     loadVisualAssets() {
         //add entity as background
-        loadImage('/img/join_room/bg.gif').then(image => {
+        loadImage('/img/game_room/forest.gif').then(image => {
             let background = new Entity(new Vec2(0, 0), image);
             this.addEntity('background', background, 0);
         });
-        //panel background
-        loadImage('/img/join_room/inputroomnumber.png').then(image => {
-            let panel = new Entity(calScaledMid(image, canvas, 0, -80), image);
-            this.addEntity('panel', panel, 1);
-        });
-        //buttons
-        loadImage('/img/join_room/joingbutton.png').then(image => {
-            let join = new Entity(calScaledMid(image, canvas, 0, -220), image);
+        //character
+        loadImage('/img/game_room/char1.png').then(image => {
+            let main = new Entity(calScaledMid(image, canvas, 0, -220), image);
             //override update method when mouse hover
-            this.addEntity('join', join, 2);
-            this.mouseBoundingBoxes['join'] = [join.position, new Vec2(join.position.x + image.width, join.position.y + image.height)];
-        });
-        loadImage('/img/join_room/createbutton.png').then(image => {
-            let create = new Entity(calScaledMid(image, canvas, 0, -460), image);
-            //override update method when mouse hover
-            this.addEntity('create', create, 2);
-            this.mouseBoundingBoxes['create'] = [create.position, new Vec2(create.position.x + image.width, create.position.y + image.height)];
-        });
-        loadImage('/img/join_room/backarrow.png').then(image => {
-            let arrow = new Entity(calScaledMid(image, canvas, 500, 300), image);
-            this.addEntity('arrow', arrow, 2);
-            this.mouseBoundingBoxes['arrow'] = [arrow.position, new Vec2(arrow.position.x + image.width, arrow.position.y + image.height)];
+            this.addEntity('main', join, 2);
         });
         
     }
+
 }
---
+++ client/js/Scenes/WaitingRoomScene.js
@@ -0,0 +1,124 @@
+import Scene from '../Scene.js';
+import { loadImage } from '../loaders.js';
+import { Entity } from '../Entity.js';
+import { Vec2, calScaledMid, getMousePos } from '../util.js';
+import TitleScene from './TitleScene.js';
+
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+export default class WaitingRoomScene extends Scene {
+
+    constructor() {
+        super();
+
+        this.loadVisualAssets();
+        //setup mouse events
+        this.setupMouseEvents();
+    }
+
+    setupMouseEvents() {
+        this.mouseClick = function onMouseClick(event) {
+            let currentPosition = getMousePos(canvas, event);
+            let boundingBoxes = event.data.extra;
+            Object.entries(boundingBoxes).forEach(entry => {
+                if(currentPosition.x >= entry[1][0].x
+                    && currentPosition.x <= entry[1][1].x
+                    && currentPosition.y >= entry[1][0].y
+                    && currentPosition.y <= entry[1][1].y
+                ) {
+                    Scene.currentScene.transition(entry[0]);
+                }
+            });    
+        }
+        this.mouseMove = function onMouseMove(event) {
+            event.preventDefault();
+            let currentPosition = getMousePos(canvas, event);
+            let boundingBoxes = event.data.extra;
+            try {
+                Object.entries(boundingBoxes).forEach(entry => {
+                    if(currentPosition.x >= entry[1][0].x
+                        && currentPosition.x <= entry[1][1].x
+                        && currentPosition.y >= entry[1][0].y
+                        && currentPosition.y <= entry[1][1].y
+                    ) {
+                        canvas.style.cursor = 'pointer';
+                        throw BreakException;
+                    } else {
+                        canvas.style.cursor = 'default';
+                    }
+                });    
+            } catch(e) {
+
+            }
+        }
+    }
+
+    transition(target) {
+        if(target === 'start') {
+            console.log('start!');
+        } else if (target === 'quit') {
+            let quit = new TitleScene();
+            quit.show();
+        }
+    }
+
+    loadVisualAssets() {
+        //add entity as background
+        loadImage('/img/wait_room/bg.gif').then(image => {
+            let background = new Entity(new Vec2(0, 0), image);
+            this.addEntity('background', background, 0);
+        });
+        //white filter
+        loadImage('/img/wait_room/1.png').then(image => {
+            let filter = new Entity(calScaledMid(image, canvas), image);
+            this.addEntity('filter', filter, 1);
+        });
+        //elements
+        loadImage('/img/wait_room/blue.png').then(image => {
+            let blue = new Entity(calScaledMid(image, canvas, 1450, 300), image);
+            this.addEntity('blue', blue, 2);
+        });
+        loadImage('/img/wait_room/pink.png').then(image => {
+            let pink = new Entity(calScaledMid(image, canvas, 500, 300), image);
+            this.addEntity('pink', pink, 2);
+        });
+        loadImage('/img/wait_room/yellow.png').then(image => {
+            let yellow = new Entity(calScaledMid(image, canvas, -500, 300), image);
+            this.addEntity('yellow', yellow, 2);
+        });
+        loadImage('/img/wait_room/green.png').then(image => {
+            let green = new Entity(calScaledMid(image, canvas, -1450, 300), image);
+            this.addEntity('green', green, 2);
+        });
+        loadImage('/img/wait_room/icepillar.png').then(image => {
+            let ice1 = new Entity(calScaledMid(image, canvas, 1450, -50), image);
+            this.addEntity('ice1', ice1, 2);
+        });
+        loadImage('/img/wait_room/icepillar.png').then(image => {
+            let ice2 = new Entity(calScaledMid(image, canvas, 500, -50), image);
+            this.addEntity('ice2', ice2, 2);
+        });
+        loadImage('/img/wait_room/icepillar.png').then(image => {
+            let ice3 = new Entity(calScaledMid(image, canvas, -500, -50), image);
+            this.addEntity('ice3', ice3, 2);
+        });
+        loadImage('/img/wait_room/icepillar.png').then(image => {
+            let ice4 = new Entity(calScaledMid(image, canvas, -1450, -50), image);
+            this.addEntity('ice4', ice4, 2);
+        });
+        //buttons
+        loadImage('/img/wait_room/start button.png').then(image => {
+            let start = new Entity(calScaledMid(image, canvas, 1450, -500), image);
+            //override update method when mouse hover
+            this.addEntity('start', start, 2);
+            this.mouseBoundingBoxes['start'] = [start.position, new Vec2(start.position.x + image.width, start.position.y + image.height)];
+        });
+        loadImage('/img/wait_room/Quitbutton.png').then(image => {
+            let quit = new Entity(calScaledMid(image, canvas, 1450, -700), image);
+            //override update method when mouse hover
+            this.addEntity('quit', quit, 2);
+            this.mouseBoundingBoxes['quit'] = [quit.position, new Vec2(quit.position.x + image.width, quit.position.y + image.height)];
+        });
+    }
+ }
