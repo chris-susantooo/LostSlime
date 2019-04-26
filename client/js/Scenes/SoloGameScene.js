2ed0a2beab2b3c7112df4adff9a62d5e2d58ca44
Rico Wan <ricowan2007@gmail.com>
2019-04-26 08:58:42 +0800

    fixed bug

--- client/js/Scenes/SoloGameScene.js
+++ client/js/Scenes/SoloGameScene.js
@@ -78,6 +78,7 @@ export default class SoloGameScene extends Scene {
                     lastMove = 'Miss';
                     moveCount[4]++;
                     round++;
+                    buffer = [];
                 }
             }
         }
